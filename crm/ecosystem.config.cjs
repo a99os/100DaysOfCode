453360d5795429bf81944f4c94d773eb37924d53
a99os <abbosashirboev@gmail.com>
2024-02-08 12:50:59 +0500

    ok

--- crm/ecosystem.config.cjs
+++ crm/ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "Nuxt-Starter",
-      port: "3000",
+      port: "3003",
       exec_mode: "cluster",
       instances: "max",
       script: "./.output/server/index.mjs",
