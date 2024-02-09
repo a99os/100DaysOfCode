fc53082970559d17dc4d4d20a5cdd0113e440201
a99os <abbosashirboev@gmail.com>
2024-02-09 10:43:36 +0500

    bug fixed at info section

--- crm/components/Group/Info.vue
+++ crm/components/Group/Info.vue
@@ -1,36 +1,37 @@
 <template lang="">
-  <div class="w-full h-[50px] bg-opcBlack border-b border-colorPrimary">
-    <ul class="flex gap-3 items-center text-[16px] justify-center h-full">
-      <b class="text-colorPrimary px-2 py-1 rounded-md bg-green">{{
-        group.name
-      }}</b>
-      <div class="h-[60%] w-[1px] bg-colorPrimary"></div>
-      <p class="text-activeColor">
-        Ustoz:
-        <b class="text-lightGreen">{{ group.teacher }}</b>
-      </p>
-      <div class="h-[60%] w-[1px] bg-colorPrimary"></div>
-      <p class="text-activeColor">
-        Fan:
-        <b class="text-lightGreen">{{ group.direction }}</b>
-      </p>
-      <div class="h-[60%] w-[1px] bg-colorPrimary"></div>
-      <p class="text-activeColor">
-        O'quvchilar soni:
-        <b class="text-lightGreen"> {{ group.studentCount }} ta </b>
-      </p>
-      <div class="h-[60%] w-[1px] bg-colorPrimary"></div>
-      <p class="text-activeColor">
-        Bo'ladigan kunlar:
-        <b class="text-lightGreen"> {{ group.weekDays }}</b>
-      </p>
-      <div class="h-[60%] w-[1px] bg-colorPrimary"></div>
-      <p class="text-activeColor">
-        Soat:
-        <b class="text-lightGreen"> {{ group.startTime }}</b>
-      </p>
-    </ul>
-  </div>
+  <ul
+    class="w-full min-h-[50px] bg-opcBlack border-b border-colorPrimary flex gap-3 items-center text-[16px] justify-center h-full"
+  >
+    <b class="text-colorPrimary h-[80%] rounded-md bg-green">{{
+      group.name
+    }}</b>
+    <div class="h-[60%] w-[1px] bg-colorPrimary"></div>
+
+    <p class="text-activeColor">
+      Ustoz:
+      <b class="text-lightGreen">{{ group.teacher }}</b>
+    </p>
+    <div class="h-[60%] w-[1px] bg-colorPrimary"></div>
+    <p class="text-activeColor">
+      Fan:
+      <b class="text-lightGreen">{{ group.direction }}</b>
+    </p>
+    <div class="h-[60%] w-[1px] bg-colorPrimary"></div>
+    <p class="text-activeColor">
+      O'quvchilar soni:
+      <b class="text-lightGreen"> {{ group.studentCount }} ta </b>
+    </p>
+    <div class="h-[60%] w-[1px] bg-colorPrimary"></div>
+    <p class="text-activeColor">
+      Bo'ladigan kunlar:
+      <b class="text-lightGreen"> {{ group.weekDays }}</b>
+    </p>
+    <div class="h-[60%] w-[1px] bg-colorPrimary"></div>
+    <p class="text-activeColor">
+      Soat:
+      <b class="text-lightGreen"> {{ group.startTime }}</b>
+    </p>
+  </ul>
 </template>
 <script setup>
 import { useRoute } from "vue-router";
