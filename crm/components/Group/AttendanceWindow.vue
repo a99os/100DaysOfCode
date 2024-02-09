6ac74326466a3c48fded914aa1ed8d6a3fdc76fb
a99os <abbosashirboev@gmail.com>
2024-02-09 15:53:35 +0500

    add responsive version pop-up window

--- crm/components/Group/AttendanceWindow.vue
+++ crm/components/Group/AttendanceWindow.vue
@@ -3,18 +3,20 @@
     id="windowWrapper"
     @click="hiddenWindow"
     :class="useGeneralStore().showAttendance ? 'block' : 'hidden'"
-    class="fixed w-[100vw] flex justify-center items-center top-0 z-[20] h-[100vh] bg-[#fffefe88] backdrop-blur-[2px]"
+    class="fixed w-[100vw] flex justify-center items-center px-2 top-0 z-[20] h-[100vh] bg-[#fffefe88] backdrop-blur-[2px]"
   >
-    <div class="h-[80%] md:min-w-[920px] rounded-md bg-bgPrimary py-10 px-14">
+    <div
+      class="h-[90%] md:h-[80%] w-full overflow-x-scroll md:min-w-[920px] rounded-md bg-bgPrimary py-5 md:py-10 px-5 md:px-14"
+    >
       <div class="flex w-full gap-10 item-center">
-        <p class="w-[100px]">Sana:</p>
+        <p class="w-[50px] md:w-[100px]">Sana:</p>
         <input
           type="Date"
           class="rounded-[7px] py-1 flex-grow px-2 outline-none border border-activeColor bg-transparent"
         />
       </div>
       <div class="flex gap-10 mt-5 item-center">
-        <p class="w-[100px]">Mavzu:</p>
+        <p class="w-[50px] md:w-[100px]">Mavzu:</p>
         <input
           type="Text"
           class="rounded-[7px] px-2 py-1 flex-grow outline-none border border-activeColor bg-transparent"
@@ -22,8 +24,10 @@
       </div>
       <ul v-if="load" class="mt-10 h-[calc(100%-180px)] overflow-y-scroll">
         <li v-for="(item, i) in students" class="flex gap-2 mt-2 items-center">
-          <p class="w-10 text-[24px]">{{ i + 1 }}.</p>
-          <p class="text-[24px] w-[400px]">{{ item.full_name }}</p>
+          <p class="w-10 md:text-[24px]">{{ i + 1 }}.</p>
+          <p class="md:text-[24px] min-w-[200px] md:min-w-[400px]">
+            {{ item.full_name }}
+          </p>
           <div class="flex flex-grow items-center">
             <button
               @click="attendanceChange('be', item.id)"
@@ -56,12 +60,12 @@
       <div class="grid items-center mt-3 grid-cols-2 py-1">
         <button
           @click="hiddenWindow2"
-          class="bg-green rounded-l-md hover:opacity-100 opacity-80 py-1 text-[24px] font-medium"
+          class="bg-green rounded-l-md px-2 hover:opacity-100 opacity-80 py-1 md:text-[24px] font-medium"
         >
           💾 Saqlash</button
         ><button
           @click="hiddenWindow2"
-          class="bg-red rounded-r-md hover:opacity-100 opacity-80 py-1 text-[24px] font-medium"
+          class="bg-red rounded-r-md px-2 hover:opacity-100 opacity-80 py-1 md:text-[24px] font-medium"
         >
           ❌ Bekor qilish
         </button>
